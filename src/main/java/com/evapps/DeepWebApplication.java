7e811bb875aede1c7246c44572e5b6cdab735292
Eduardo Veras <eduardo.storm@gmail.com>
2016-11-22 15:00:42 -0400

    First Commit, empty proyect

---
+++ src/main/java/com/evapps/DeepWebApplication.java
@@ -0,0 +1,12 @@
+package com.evapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepWebApplication.class, args);
+	}
+}
