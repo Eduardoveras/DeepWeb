1538e7447a87645ec7efde163b646ba8ca7ad143
Djsiclait <djidjellypas92@gmail.com>
2016-11-26 14:59:38 -0400

    Refactor History Repository

--- src/main/java/com/evapps/Repository/HistoryRepository.java
+++ src/main/java/com/evapps/Repository/HistoryRepository.java
@@ -8,19 +8,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 
 public interface HistoryRepository extends JpaRepository<History, String> {
 
     History findByHistoryId(String historyId);
 
-    @Query("select h from History h where h.patient.patientId = :id and h.consultationReference = :reference")
-    History findByPatientIdAndConsultationReference(@Param("id") String patientId, @Param("reference") String consultationReference);
-
-    @Query("select h from History h where h.patient.patientId = :id" )
-    List<History> findByPatientId(@Param("id") String patientId);
-
-    @Query("select h from History h where h.surgeryType = :surgeryType" )
-    List<History> findBySurgeryType(@Param("surgeryType") String surgeryType);
-
+    @Query("select h from History h where h.user.email = :email")
+    History findByUser(@Param("email") String email);
 }
