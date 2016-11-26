f0d8e11a4bedc2e11dfea6cc15c8f9c609235ef4
Djsiclait <djidjellypas92@gmail.com>
2016-11-26 16:24:16 -0400

    Add default admins: The cool guys

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (email, first_name, last_name, password, role, shipping_address)
+VALUES ('dj@gmail.com', 'Djidjely', 'Siclait', '1234', 0, 'nowhere');
+
+INSERT INTO users (email, first_name, last_name, password, role, shipping_address)
+VALUES ('ev@gmail.com', 'Eduardo', 'Veras', '1234', 0, 'nowhere');
+
+INSERT INTO users (email, first_name, last_name, password, role, shipping_address)
+VALUES ('er@gmail.com', 'Ernesto', 'Rodriguez', '1234', 0, 'nowhere');
