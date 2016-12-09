a1d238053cd1ebdc6fe213a0d835f6b8488a0184
Eduardo Veras <eduardo.storm@gmail.com>
2016-12-09 16:53:06 -0400

    rest

--- src/main/java/com/evapps/Controller/AppController.java
+++ src/main/java/com/evapps/Controller/AppController.java
@@ -4,31 +4,18 @@ import com.evapps.Entity.Receipt;
 import com.evapps.Service.CRUD.ReadDataService;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by Eduardo veras on 12/7/2016.
  */
 
 @RestController
-public class AppController {
-    ReadDataService RDS;
+class GreetingController {
+    private static ReadDataService RDS;
 
-    @RequestMapping(value ="/greeting", method = RequestMethod.GET, produces = "application/json")
-    public @ResponseBody  List<Receipt> greeting(@RequestParam(value="name", defaultValue="World") String name) {
-
-        List<Receipt> test= RDS.findAllRegisteredTransactions();
-        if (RDS.findAllRegisteredTransactions() == null)
-        {
-            test= new ArrayList<>();
-            return test;
-        }
-
-
-
-        return test;
-
+    @GetMapping("/greeting")
+    public static List<Receipt> greeting(@RequestParam(value = "name", defaultValue = "World") String name){
+        return RDS.findAllRegisteredTransactions();
     }
 }
